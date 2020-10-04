d92978806fff1f6c5863422fbe8b9ee32e679b11
jinnkeyxx <68957615+jinnkeyxx@users.noreply.github.com>
2020-10-04 15:14:24 +0700

    a
    a

--- app/Views/admin/templates/navbar.php
+++ app/Views/admin/templates/navbar.php
@@ -10,17 +10,55 @@
                         <li class="menu-title">Navigation</li>
 
                         <li>
-                            <a href="index.html">
-                                <i class="mdi mdi-view-dashboard"></i>
-                                <span> Dashboard </span>
+                            <a href="javascript: void(0);">
+                                <i class="mdi mdi-google-pages"></i>
+                                <span> Settings </span>
+                                <span class="menu-arrow"></span>
                             </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="setting-meta">Setting Meta</a></li>
+                                <li><a href="setting-header">Setting Header</a></li>
+                                <li><a href="setting-footer">Setting Footer</a></li>
+                                <li><a href="setting-info">Setting Info</a></li>
+                                
+                            </ul>
                         </li>
 
                         <li>
-                            <a href="calendar.html">
-                                <i class="mdi mdi-calendar-month"></i>
-                                <span> Calendar </span>
+                            <a href="javascript: void(0);">
+                                <i class="mdi mdi-google-pages"></i>
+                                <span> Bài viết </span>
+                                <span class="menu-arrow"></span>
                             </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="write-post">Viết bài mới</a></li>
+                                <li><a href="post">Tất cả bài viết</a></li>
+                                
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);">
+                                <i class="mdi mdi-google-pages"></i>
+                                <span> Quản lí bình luận </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                
+                                <li><a href="post">Tất cả bình luận</a></li>
+                                
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);">
+                                <i class="mdi mdi-google-pages"></i>
+                                <span> Quản lí Contacts </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li><a href="post">Tất cả Contacts</a></li>
+                            </ul>
                         </li>
 
                        
